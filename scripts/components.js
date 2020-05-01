da0fe0e1c2d7e055af55daf4f1f127b5f74546a4
Nyandereian <nyandereian@gmail.com>
2020-05-02 01:22:33 +0200

    Add explanatory comments

--- scripts/components.js
+++ scripts/components.js
@@ -1,5 +1,6 @@
 // Starting animation
 (() => {
+  // Apply visual fade-in effects
   let title = document.querySelector('.title h1');
   let form = document.querySelector('.form-wrapper');
   title.addEventListener('animationend', e => {
@@ -8,6 +9,8 @@
     form.style.opacity = '1';
     form.style.transform = 'translateY(0)';
   });
+
+  // Autofocus on player 1 name input for an efficient flow
   title.parentNode.addEventListener('webkitTransitionEnd', e => {
     setTimeout(() => form.firstElementChild[name="player-name"].focus(), 300);
   })
@@ -15,13 +18,14 @@
 
 
 // Form interface
+// Tell user to enter name
 Array.from(document.querySelectorAll('.name'))
      .forEach(input => {
        input.addEventListener('focus', (e) => e.target.placeholder = "Name");
        input.addEventListener('focusout', (e) => e.target.placeholder = "Player");
      });
 
-
+// Create smooth UX when selecting the player 2 option
 (() => {
  let radio = document.querySelector('#player2');
  let input = document.querySelector('input[name="player2-name"]');
@@ -35,19 +39,25 @@ document.querySelector('.player-form').addEventListener('submit', e => {
   e.preventDefault();
   let form = e.target;
 
+  // Disable form elements so they can't be changed / clicked during animations
   Array.from(form.elements).forEach(input => input.disabled = true);
 
+  // Smoothly remove start button through transitions
   let submit = form.start;
   submit.classList.toggle('clicked');
   submit.style.cursor = 'default';
   submit.style.opacity = 0;
 
+  // Remove radio buttons too
   let radioButtons = Array.from(document.querySelectorAll('.radio'));
   radioButtons.forEach(radio => radio.style.opacity = '0');
 
+  // Give default name when no input is given for user-friendliness
   let player1 = form['player-name'];
   player1.value = player1.value || 'Player 1';
 
+  // Check selected opponent and remove the other to get a vs screen
+  // Adjust top / bottom for horizontal text alignment
   let opponents = document.querySelector('.opponents');
   let player2Radio = form['player2'];
   let player2 = form['player2-name'];
@@ -65,16 +75,20 @@ document.querySelector('.player-form').addEventListener('submit', e => {
     var checked = bot.parentNode.querySelector('.name');
   }
 
+  // Wait for transitions to end before hiding the visibility
+  // for a fade-out effect
   setTimeout(() => {
     submit.style.visibility = 'hidden';
     radioButtons.forEach(radio => radio.style.visibility = 'hidden');
     unchecked.style.visibility = 'hidden';
+    // Emphasize the opponents on the vs screen
     player1.style.transform = 'scale(1.5)';
     checked.style.transform = 'scale(1.5)';
   }, 300);
 
   let title = document.querySelector('.title h1');
 
+  // Apply effects for smooth transitioning to the play screen
   setTimeout(() => {
     title.style.opacity = '0';
     title.style.transform = '1.5';
